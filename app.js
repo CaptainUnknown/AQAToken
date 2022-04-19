2e4cad83e7980488be5bb790857a6a9c642ee1d9
CaptainUnknown <79987654+CaptainUnknown@users.noreply.github.com>
2022-04-20 02:13:04 +0500

    Created app.js for Server

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const port = 3000
+
+const server = http.createServer(function(req, res) {
+    res.ret('Hello node')
+    res.end()
+
+})
+
+server.listen(port, function(error){
+    if (error){
+        console.log('Something went wrong', error)
+    }
+    else {
+        console.log('Server Listening on port ' + port)
+    }
+})
